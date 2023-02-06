885d9780b479bfe1c081c2bb6e05dcd2ff8eebb3
hhklik <37667605+hhklik@users.noreply.github.com>
2023-02-05 19:11:29 -0600

    Se agregaron nuevas caracteristicas

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ chmod +x /usr/local/bin/chapi
 if [ -d /usr/local/etc/chapi.d ]; then
   echo "chapi.d is already exist"
 else
-  cp -rf chapi.d /usr/local/etc/chapi.d
+  cp -rf . /usr/local/etc/chapi.d
 fi
 chapi_home=$(grep "CHAPI_HOME=" /etc/environment | awk '{print $1}')
 if [ -z $chapi_home ]; then
